909aae3691be33f3dd38709a83ae082a74748633
Twizzay <gandalf@mylegendary.quest>
2023-01-20 23:43:45 -0800

    Added some new lines to the story  Also added some graphical controls dependant on input values.  Added some input sanitization
     Changes to be committed:
    	modified:   main.js
    	modified:   page1.html
    	modified:   styles.css

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
 "use strict";
 const body = document.querySelector('body');
-let duck = document.querySelector("input[name='duck_color_name']");
-let duckObject = {}
-let result = document.querySelector('p.result');
+let duckInput = document.querySelector("input[name='duck_color_name']");
+let duck = {};
+let phaseOne = document.querySelector('p.phaseOne');
 
-function colorizeBG() {
-   document.querySelector("body").style.backgroundColor = duck.value;
+let colorizeDuck = function saveDuckColorToObject() {
+   duckInput.style.backgroundColor = duckInput.value;
    //saves input to memory.
-   duckObject.color = duck.value;
+   duck.color = duckInput.value;
 }
 
 document.addEventListener('keyup', (event) => {
    const keyName = event.key;
    // listens for Enter and then... 
    if (keyName === 'Enter') {
-      //Colors the BG based on the input...
-      colorizeBG();
-      duck.value = null;
-      result.textContent = "Hmmmmmmm...."
+      //Colors the Duck based on the input...
+      colorizeDuck();
+      phaseOne.innerHTML = `<p class="phaseOne">Hmmmmmmm....</p>`
       // And checks to see if it was a valid color.
-      let fn = () => {
-         let currentBGStyle = window.getComputedStyle(body).getPropertyValue("background-color");
-         let regex = /rgba/;
-         let match = currentBGStyle.match(regex);
-         if (Array.isArray(match)) {
-         result.textContent = "No, that wasn't it.";
-         };
-      result.textContent = `Ah, yes. It was definitely a ${duckObject.color} duck.`;
+      let checkInput = () => {
+         let currentDuckStyle = window.getComputedStyle(duckInput).getPropertyValue("background-color");
+         let regex = /rgb\(255, 255, 255\)/g;
+         let match = currentDuckStyle.match(regex);
+         console.log(currentDuckStyle);
+         console.log(match);
+         if (match) {
+         phaseOne.innerHTML = `<p class="phaseOne">No, that wasn't it...</p>`;
+         }
+         else {
+         phaseOne.innerHTML = `<p class="phaseOne">Ah, yes. It was definitely a ${duck.color} duck.</p>`;
+         }
       };
-   setTimeout(fn, 3000);
-      
+   setTimeout(checkInput, 3000);
+      //MAKE A CONSTRUCTOR FUNCTION FOR THESE
+   let phaseTwoFN = function showPhaseTwoText() {
+      let phaseTwo = document.querySelector('p.phaseTwo');
+      phaseTwo.style.display = "block";
+   }
+   let phaseThreeFN = function showPhaseThreeText() {
+      let phaseThree = document.querySelector('div.phaseThree');
+      phaseThree.style.display = "block";
+   }
+   setTimeout(phaseTwoFN, 6000);
+   setTimeout(phaseThreeFN, 8000);
    };
 });
