e08b682fb1d1a49ff6e153272dbeb082c59450d5
Twizzay <gandalf@mylegendary.quest>
2023-02-06 17:50:24 -0800

    Adding to the story. Optimizations etc.  Changes to be committed: 	new file:   css/animate_rain.css 	modified:   css/animate_water_flow.css 	modified:   css/styles.css 	modified:   js/main.js 	modified:   js/utils.js 	modified:   page1.html

--- js/main.js
+++ js/main.js
@@ -1,67 +1,64 @@
 "use strict";
-//duck Object for duck quality persistance
-const duck = {
-	water: document.querySelectorAll("pre.water_flow"),
-	illust: document.querySelector("pre#duck"),
-};
-//commonly used tags
-const page = {
-	body:  document.querySelector("body"),
-	container: document.getElementsByClassName("container"),
-	duckInput: document.querySelector("input#duck_color")
-};
-const phase = function(tag) {
-	this.tag = document.querySelector(tag),
-	this.fn = function() {
-		this.tag.style.visibility = "unset";
-		this.tag.style.opacity = 1;
-	}
-//	this.transit = function(arg) {
-//		this.tag.addEventListener("transitionend", (arg) => arg);
-//	},
-//	this.timeout = function() {
-//		setTimeout(this.fn.bind(this), time)
-//	}
-};
 //fires after enter is hit
 document.addEventListener('keyup', (event) => {
 	const keyName = event.key;
 	if (keyName === "Enter") {
-		letUsBegin();
+		storyStart();
 	};
 });
 
-const letUsBegin = function fireTheShootyGunRay(){
+
+const storyStart = function storyStart(){
 	const phaseOne = new phase("div.storyStart");
 	//Colors the Duck based on the input.
 	const colorizeDuck = function saveDuckColorToObject() {
 		page.duckInput.style.backgroundColor = page.duckInput.value;
-		//saves input to memory.
 		duck.color = page.duckInput.value;
+		//saves input to memory.
+		return duck.color;
 		}
 	phaseOne.tag.innerHTML = `<p class="storyStart">Hmmmmm.... </p>`;
 	phaseOne.fn();
 	colorizeDuck();
+	// verifies the input.
 	page.duckInput.addEventListener("transitionend", () => {
-		const checkInput = () => {
 			page.duckInput.disabled = true;
 			phaseOne.tag.innerHTML = `<p class="storyStart">Ah, yes. It was definitely a ${duck.color} duck.</p>`;
 			phaseOne.fn();
-		};
-			checkInput();
 	});
 }
+
+
 page.duckInput.addEventListener("transitionend", () => {
 	const phaseTwo = new phase("div.phaseTwo");
+	let eyes = [document.querySelector("pre#phTwo_pt2"), document.querySelector("pre#phTwo_pt3")]
+	page.body.style.backgroundColor = "#888"
 	setTimeout(() => {
 		phaseTwo.fn();
-		let eyes = [document.querySelector("pre#two"), document.querySelector("pre#three")]
 		for (let i of eyes)
 			i.style.visibility = "collapse";
 	for (let i of page.container)
 	i.style.flex = 1;
 	}, 1000);
-})
+	page.container[0].addEventListener("transitionrun", () => {
+		eyes[0].style.visibility = "unset";
+		eyes[0].style.fontSize = "30pt";
+		setTimeout(() => {
+		eyes[0].style.visibility = "collapse";
+		}, 2000);
+		setTimeout(() => {
+			eyes[1].style.visibility = "unset";
+			eyes[1].style.fontSize = "30pt";
+		}, 3000);
+		setTimeout(() => {
+		eyes[1].style.visibility = "collapse";
+		}, 5000);
+		
+		let document.querySelector(`#rain_${randomNum(10)}`);
+		//need to assign display randomly so that drops fall in random order.
+	});
+});
+
 
 //	phaseTwo.timeout();
 //	const phaseThree = new phase("phaseThree", "div", 8000);
--- js/utils.js
+++ js/utils.js
@@ -1,3 +1,36 @@
+"use strict";
+//duck Object for duck quality persistance
+const duck = {
+	water: document.querySelectorAll("pre.water_flow"),
+	illust: document.querySelector("pre#duck"),
+};
+//let testerButton = 
+//commonly used tags
+const page = {
+	body:  document.querySelector("body"),
+	container: document.getElementsByClassName("container"),
+	duckInput: document.querySelector("input#duck_color")
+};
+const phase = function(tag) {
+	this.tag = document.querySelector(tag),
+	this.fn = function() {
+		this.tag.style.visibility = "unset";
+		this.tag.style.opacity = 1;
+	}
+//	this.transit = function(arg) {
+//		this.tag.addEventListener("transitionend", (arg) => arg);
+//	},
+//	this.timeout = function() {
+//		setTimeout(this.fn.bind(this), time)
+//	}
+};
+
+const randomNum = function randomNumberGenerator(max){
+	return Math.floor(Math.random(max));
+}
+
+
+
 //let reload = function tabReloader(){
 //	location.reload();
 //}
