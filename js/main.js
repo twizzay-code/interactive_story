cd7f2b1e47eb9e5fe6629e436a963e3b4c4f1fa9
Twizzay <gandalf@mylegendary.quest>
2023-02-13 18:03:39 -0800

    random array has been succesfully created. On to asyncronously dropping the raindrops. Learning about async/await with promises next.
     Changes to be committed:
    	modified:   css/animate_rain.css
    	modified:   js/main.js

--- js/main.js
+++ js/main.js
@@ -9,8 +9,8 @@ document.addEventListener('keyup', (event) => {
 
 
 const storyStart = function storyStart(){
-	const phaseOne = new phase("div.storyStart"),
 	//Colors the Duck based on the input.
+	const phaseOne = new phase("div.storyStart"),
 	colorizeDuck = function saveDuckColorToObject() {
 		page.duckInput.style.backgroundColor = page.duckInput.value;
 		duck.color = page.duckInput.value;
@@ -75,64 +75,9 @@ page.duckInput.addEventListener("transitionend", () => {
 					rainSelector.style.opacity = 1;
 				}, rainArray[i/3] * 1000)
 			}
+			// ^^^ this still isn't working as it should. Need to look into how to make a loop delay after each iteration.
+			// take a look at async/await stuff using promises.
 		};
 		makeItRain();
 	});
 });
-
-
-
-// ALRIGHT screw the random number generator just use this:
-// const rainOrder = [5,9,7,2,4,1,6,10,8,3]
-// have the makeItRain function iterate through this array after a one second delay on each item.
-// If you really want to you can look at how to generate a unique array with no repeating numbers again.
-//
-// You are getting hung up on this promise await statement stuff though.
-// generate a workable list and THEN iterate through it.
-// stop trying to generate the list AS you iterate through it.
-//
-// This was, still, a lot of fun.
-// 
-
-
-
-// OLD PLAN BELOW
-//		const repeats = [];
-//		let ranNum = randomNum(9) + 1;
-//		const rainDrops = 10;
-//		const rainRep = function raindropRepeaterFn(rainfall){
-//			console.log(repeats);
-//				if (repeats.some((e) => {e === ranNum})){
-//					ranNum = randomNum(9) + 1;
-//					rainRep();
-//					}
-//				else {
-//					const ranRain = document.querySelector(`#rain_${ranNum}`);
-//					ranRain.style.display = "unset";
-//					ranRain.style.opacity = 1;
-//					rainfall(ranNum);
-//				}
-//		}
-//		const promRep = function promiseMeAgain(){
-//			const rainWillFall = new Promise((rainfall) => {
-//				setTimeout(() => {rainRep(rainfall)}, 1000);
-//			})
-//			rainWillFall.then((passedValue) => {
-//				repeats.push(passedValue);
-//				while (repeats.length < rainDrops){
-//					ranNum = randomNum(9) + 1;
-//					promRep();
-//				};
-//			});
-//		}
-//		promRep();
-
-
-
-//	phaseTwo.timeout();
-//	const phaseThree = new phase("phaseThree", "div", 8000);
-//	phaseThree.timeout();
-//	duck.illust.style.color = duck.color;
-//	for (let i of duck.water){
-//	i.style.color = "blue";
-//	}
